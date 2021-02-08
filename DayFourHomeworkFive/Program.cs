4b1380c0f2c1fb94d4a3e4d5a6d32d6ae5c2596e
Alihan Girgin <alihangir@gmail.com>
2021-02-09 02:45:20 +0300

    Typo fixed

--- DayFourHomeworkFive/Program.cs
+++ DayFourHomeworkFive/Program.cs
@@ -8,17 +8,19 @@ namespace DayFourHomeworkFive
     {
         static void Main(string[] args)
         {
+            //You can use <int,int>,<int,string>,<string,string>,<string,int>
             MyEditor<int, string> dictionary = new MyEditor<int,string>();
-            dictionary.Add(3, "YARRRR");
+            dictionary.Add(3, "YAPRAK");
             dictionary.Add(1, "AA");
             dictionary.Add(2, "CC");
             dictionary.Add(1, "BB");
             dictionary.Add(2, "DD");
+            dictionary.Add(1, "MMMMM");
 
-       
+
             foreach (var item in dictionary.Dictionary)
             {
-                Console.Write("Key:{" + item.Key + "}-" );
+                Console.Write("Key:{" + item.Key + "} -> " );
                 var iteration = 1;
                 foreach (var value in item.Value)
                 {
